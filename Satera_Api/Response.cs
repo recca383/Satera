ef96cabca50364f7b9bf2f70c42ee3c1e1c14b04
recca383 <sirpatrick121402@gmail.com>
2026-01-06 20:06:42 +0800

    Added HTTP Response class

---
+++ Satera_Api/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace Satera_Api
+{
+    public sealed record Response(
+        float AcademicAtRiskScore,
+        float AverageBalancedUserScore,
+        float DigitalMultitaskerScore,
+        float DigitalSelfRegulatedScore,
+        float HighFunctioningAcademicScore,
+        float MinimalDigitalengagerScore,
+        DateTime DateAnalyzed
+        );
+}
