76f5e8ebc752ace74c01258f30bc78a8ef545adc
recca383 <sirpatrick121402@gmail.com>
2026-01-06 20:06:18 +0800

    Revised HTTP Request class

--- Satera_Api/Request.cs
+++ Satera_Api/Request.cs
@@ -1,4 +1,21 @@
 namespace Satera_Api
 {
-    public sealed record Request(string name);
+    public sealed record Request(
+        float Gwa,
+        int TrackingDurationDays,
+        int TotalScreenTime,
+        int TotalAppsTracked,
+        int Pickups,
+        int DeviceUnlocks,
+        App[] Apps,
+        string CollectionTimestamp,
+        string Platform
+    );
+
+    public sealed class App
+    {
+        public required string PackageName { get; set; }
+        public int TotalTimeInForeground { get; set; }
+    }
+
 }
