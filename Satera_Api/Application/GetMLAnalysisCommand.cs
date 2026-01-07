54314ce832386e403f4855177b3fddb9983aae3e
recca383 <sirpatrick121402@gmail.com>
2026-01-07 20:11:59 +0800

    Modified ML Analysis Command

--- Satera_Api/Application/GetMLAnalysisCommand.cs
+++ Satera_Api/Application/GetMLAnalysisCommand.cs
@@ -1,4 +1,14 @@
 ﻿namespace Satera_Api.Application
 {
-    public record GetMLAnalysisCommand(string name);
+    public record GetMLAnalysisCommand(
+        float Gwa,
+        int TrackingDurationDays,
+        int TotalScreenTime,
+        int TotalAppsTracked,
+        int Pickups,
+        int DeviceUnlocks,
+        App[] Apps,
+        string CollectionTimestamp,
+        string Platform
+        );
 }
